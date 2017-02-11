49e66e3e8da94b2597e18ccc98512dc10902220e
Michael TRUONG <michael.truong.2012@gmail.com>
2017-02-12 01:03:13 +1100

    add more tests for calculation

--- src/test/java/net/mnb/codeprojects/javase/tenpinbowlingscore/BowlingScoreBoardVerifyingScoreCalculationTest.java
+++ src/test/java/net/mnb/codeprojects/javase/tenpinbowlingscore/BowlingScoreBoardVerifyingScoreCalculationTest.java
@@ -14,4 +14,31 @@ public class BowlingScoreBoardVerifyingScoreCalculationTest {
 
         assertEquals(300, finalScore);
     }
+
+    @Test
+    public void shouldReturn10ForRollScores_1_2_3_4() throws Exception {
+        BowlingScoreBoard board = new BowlingScoreBoard(new String[]{"1", "2", "3", "4"});
+
+        int finalScore = board.calculateFrameScores();
+
+        assertEquals(10, finalScore);
+    }
+
+    @Test
+    public void shouldReturn29ForRollScores_9_1_9_1() throws Exception {
+        BowlingScoreBoard board = new BowlingScoreBoard(new String[]{"9", "1", "9", "1"});
+
+        int finalScore = board.calculateFrameScores();
+
+        assertEquals(29, finalScore);
+    }
+
+    @Test
+    public void shouldReturn18ForRollScores_1_1_1_1_10_1_1() throws Exception {
+        BowlingScoreBoard board = new BowlingScoreBoard(new String[]{"1", "1", "1", "1", "10", "1", "1"});
+
+        int finalScore = board.calculateFrameScores();
+
+        assertEquals(18, finalScore);
+    }
 }
