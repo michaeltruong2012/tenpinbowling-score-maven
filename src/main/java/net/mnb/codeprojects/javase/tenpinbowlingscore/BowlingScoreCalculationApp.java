c78e37820af64d5f353f9f8cbffbc3d3955bb1de
Michael Truong <michael.truong.2012@gmail.com>
2017-02-13 06:26:52 +1100

    Update java doc

--- src/main/java/net/mnb/codeprojects/javase/tenpinbowlingscore/BowlingScoreCalculationApp.java
+++ src/main/java/net/mnb/codeprojects/javase/tenpinbowlingscore/BowlingScoreCalculationApp.java
@@ -4,11 +4,7 @@ package net.mnb.codeprojects.javase.tenpinbowlingscore;
  * The main application which calculates the final score of a ten-pin bowling game.
  * The scoring rules can be found in the <a href="http://www.bowling2u.com/trivia/game/scoring.asp">Scoring</a> page.
  * <p>
- * <b>Syntax</b>
- * <pre>BowlingScoreCalculationApp "roll-score-1 roll-score-2 ... roll-score-N"</pre>
- * <i>Where</i> N in [1..10]
- *
- * <b>NOTE</b>: If N < 10, all frames from N+1 to 10 are deemed 0.
+ * See project README.md for detailed description and references.
  *
  * @author Michael Truong
  */
